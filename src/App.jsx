527f386d65893893f5b840d484b8c32aa7659616
Gagan K <gagan.innov.con@phonepe.com>
2023-10-01 11:55:11 +0530

    created tambola number pick on random basis

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,91 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 
+const getInitialGrid = () => {
+  let numbers = new Array();
+  for(let i=0; i<9; i++) {
+    let nums = new Array();
+    for(let j=(10*i)+1; j<=10*(i+1); j++) {
+      let obj = {
+        "tick": false,
+        "value": j
+      }
+      nums.push(obj);
+    }
+    numbers.push(nums);
+  }
+  return numbers;
+}
+
+const createNumbers = () => {
+  const numbers = new Array();
+  for(let i=1; i<=90; i++) {
+    numbers.push(i);
+  }
+  return numbers;
+}
+
+const shuffleNumbers = (numbers) => {
+  for (let i = numbers.length - 1; i > 0; i--) {
+    const j = Math.floor(Math.random() * (i + 1));
+    [numbers[i], numbers[j]] = [numbers[j], numbers[i]]; // Swap elements at i and j
+  }
+  return numbers;
+}
+
 function App() {
-  const [count, setCount] = useState(0)
+  const [grid, setGrid] = useState(() => getInitialGrid());
+  const [availableNums, setAvailableNums] = useState(() => shuffleNumbers(createNumbers()));
+  const [currentNum, setCurrentNum] = useState(0);
+
+  const pickNumber = (availableNums) => {
+    const randomIndex = Math.floor(Math.random() * availableNums.length);
+    const randomNumber = availableNums[randomIndex];
+    console.log(randomIndex, randomNumber)
+    setCurrentNum(randomNumber);
+    availableNums.splice(randomIndex, 1)[0];
+    setAvailableNums(shuffleNumbers(availableNums));
+    setGrid(grid => {
+      return grid.map(row => {
+        return row.map(column => {
+          return column.value === randomNumber ? {...column, tick: true} : column
+        })
+      })
+    })
+  }
+
+  const resetNumber = () => {
+    setAvailableNums(shuffleNumbers(createNumbers()));
+    setCurrentNum(0)
+    setGrid(grid => {
+      return grid.map(row => {
+        return row.map(column => {
+          return {...column, tick: false}
+        })
+      })
+    })
+  }
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <div className='main'>
+      <h1 className='header'>Tambola</h1>
+      <table>
+        <tbody>
+          {grid.map((row,i) => {
+            return <tr key={i}>
+              {row.map((column,j) => {
+                return <td key={j} className={column.tick ? "ticked" : "unticked"} >{column.value}</td>
+              })}
+            </tr>
+          })}
+        </tbody>
+      </table>
+      <h1>{currentNum}</h1>
+      <div className='buttonContainer'>
+        <button className='pickNumber' onClick={() => pickNumber(availableNums)}>Pick Number</button>
+        <button className='resetNumber' onClick={() => resetNumber()}>Reset</button>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    </div>
   )
 }
 
